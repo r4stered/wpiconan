060253524969a130a261a8c5742a5a92e5cf2e55
Drew Williams <williams.r.drew@gmail.com>
2023-03-30 15:46:25 -0400

    generalized build class for wpi

--- wpicommon/conanfile.py
+++ wpicommon/conanfile.py
@@ -1,4 +1,7 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.build import can_run
+import os
 
 class WpiCommon:
     def generate_download_urls(self, library_name, version, os, arch, shared, debug):
@@ -19,6 +22,30 @@ class WpiCommon:
         lib_url = base_url + f"{_os}{_arch}{static_str}{debug_str}.zip"
 
         return (header_url, lib_url)
+    
+class testPackageBase:
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_exec")
+            self.run(cmd, env="conanrun")
 
 class WpiCommonPkg(ConanFile):
     name = "wpicommon"
