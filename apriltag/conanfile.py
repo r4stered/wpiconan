fc7af635a47fb25b0d40dceb36963d4142adc4cc
Drew Williams <williams.r.drew@gmail.com>
2023-04-15 14:19:59 -0400

    add april tag lib

---
+++ apriltag/conanfile.py
@@ -0,0 +1,11 @@
+from conan import ConanFile
+
+
+class apriltagRecipe(ConanFile):
+    name = "apriltag"
+    version = "2023.4.3"
+    python_requires = "wpicommon/0.1"
+    python_requires_extend = "wpicommon.WpiCommon"
+
+    def requirements(self):
+        self.requires("wpimath/2023.4.3", transitive_headers=True, transitive_libs=True)
---
+++ apriltag/test_package/src/test.cpp
@@ -0,0 +1,7 @@
+#include "frc/apriltag/AprilTagDetector.h"
+
+int main(int argc, char** argv) {
+  frc::AprilTagDetector detector;
+  detector.AddFamily("tag16h5");
+  detector.SetConfig({.refineEdges = false});
+}
