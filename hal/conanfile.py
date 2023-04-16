c3ecdc240d2b36d2d71315284dee1fe774dd6c62
Drew Williams <williams.r.drew@gmail.com>
2023-04-15 21:58:14 -0400

    added dl lib

--- hal/conanfile.py
+++ hal/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.files import collect_libs
 
 
 class halRecipe(ConanFile):
@@ -11,3 +12,8 @@ class halRecipe(ConanFile):
         if self.options.target == "armv7":
             self.requires("nilibraries/2023.3.0", transitive_headers=True, transitive_libs=True)
         self.requires("wpiutil/2023.4.3", transitive_headers=True, transitive_libs=True)
+
+    def package_info(self):
+        self.cpp_info.libs = collect_libs(self)
+        if str(self.settings.os) == "Linux":
+            self.cpp_info.system_libs = ["pthread", "dl"]
