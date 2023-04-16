25c0b1cc7dc31a11bb336f3d703e01aa061c8920
Drew Williams <williams.r.drew@gmail.com>
2023-04-15 22:01:02 -0400

    added atomic lib to hal

--- hal/conanfile.py
+++ hal/conanfile.py
@@ -16,4 +16,4 @@ class halRecipe(ConanFile):
     def package_info(self):
         self.cpp_info.libs = collect_libs(self)
         if str(self.settings.os) == "Linux":
-            self.cpp_info.system_libs = ["pthread", "dl"]
+            self.cpp_info.system_libs = ["pthread", "dl", "atomic"]
