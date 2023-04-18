7c3b3c484e418a02ccaa89d3404590eb3d5c4022
Drew Williams <williams.r.drew@gmail.com>
2023-04-18 18:41:23 -0400

    added req for wpilibc for phoenix

--- ctre/conanfile.py
+++ ctre/conanfile.py
@@ -9,6 +9,9 @@ class ctreRecipe(ConanFile):
     python_requires = "wpicommon/0.1"
     python_requires_extend = "wpicommon.WpiCommon"
 
+    def requirements(self):
+        self.requires("wpilibc/2023.4.3", transitive_headers=True, transitive_libs=True)
+
     def build(self):
         tools_header_url, tools_lib_url = super().generate_ctre_url(
             "tools",
