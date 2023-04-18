193185bc86483be8795f07c3a6b27946a4f2a6cf
Drew Williams <williams.r.drew@gmail.com>
2023-04-18 18:38:02 -0400

    added condition for sim libs

--- ctre/conanfile.py
+++ ctre/conanfile.py
@@ -43,38 +43,39 @@ class ctreRecipe(ConanFile):
         get(self, cci_header_url)
         get(self, cci_lib_url)
 
-        simapicpp_header_url, simapicpp_lib_url = super().generate_ctre_url(
-            "api-cpp-sim",
-            self.version,
-            str(self.settings.os),
-            str(self.settings.arch),
-            str(self.settings.build_type),
-            True
-        )
-        get(self, simapicpp_header_url)
-        get(self, simapicpp_lib_url)
+        if self.options.target is None:
+            simapicpp_header_url, simapicpp_lib_url = super().generate_ctre_url(
+                "api-cpp-sim",
+                self.version,
+                str(self.settings.os),
+                str(self.settings.arch),
+                str(self.settings.build_type),
+                True
+            )
+            get(self, simapicpp_header_url)
+            get(self, simapicpp_lib_url)
 
-        ccisim_header_url, ccisimcpp_lib_url = super().generate_ctre_url(
-            "cci-sim",
-            self.version,
-            str(self.settings.os),
-            str(self.settings.arch),
-            str(self.settings.build_type),
-            True
-        )
-        get(self, ccisim_header_url)
-        get(self, ccisimcpp_lib_url)
+            ccisim_header_url, ccisimcpp_lib_url = super().generate_ctre_url(
+                "cci-sim",
+                self.version,
+                str(self.settings.os),
+                str(self.settings.arch),
+                str(self.settings.build_type),
+                True
+            )
+            get(self, ccisim_header_url)
+            get(self, ccisimcpp_lib_url)
 
-        wpiapisim_header_url, wpiapisim_lib_url = super().generate_ctre_url(
-            "wpiapi-cpp-sim",
-            self.version,
-            str(self.settings.os),
-            str(self.settings.arch),
-            str(self.settings.build_type),
-            True
-        )
-        get(self, wpiapisim_header_url)
-        get(self, wpiapisim_lib_url)
+            wpiapisim_header_url, wpiapisim_lib_url = super().generate_ctre_url(
+                "wpiapi-cpp-sim",
+                self.version,
+                str(self.settings.os),
+                str(self.settings.arch),
+                str(self.settings.build_type),
+                True
+            )
+            get(self, wpiapisim_header_url)
+            get(self, wpiapisim_lib_url)
 
         wpiapi_header_url, wpiapi_lib_url = super().generate_ctre_url(
             "wpiapi-cpp",
