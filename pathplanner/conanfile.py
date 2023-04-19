b07ce8b0e969eda27d7caccf06a5c24829298c62
Drew Williams <williams.r.drew@gmail.com>
2023-04-18 21:00:14 -0400

    added new command to pathplanner lib

--- pathplanner/conanfile.py
+++ pathplanner/conanfile.py
@@ -9,7 +9,7 @@ class pathplannerRecipe(ConanFile):
     python_requires_extend = "wpicommon.WpiCommon"
 
     def requirements(self):
-        self.requires("wpilibc/2023.4.3", transitive_headers=True, transitive_libs=True)
+        self.requires("wpilibnewcommands/2023.4.3", transitive_headers=True, transitive_libs=True)
 
     def build(self):
         pathplanner_header_url, pathplanner_lib_url = super().generate_pathplanner_url(
