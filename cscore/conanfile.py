333013a42bcc460acb503a70b02991518ceebdfe
Drew Williams <williams.r.drew@gmail.com>
2024-04-10 09:48:23 -0400

    added cscore

---
+++ cscore/conanfile.py
@@ -0,0 +1,38 @@
+from conan import ConanFile
+from conan.tools.files import get
+
+
+class CscoreConan(ConanFile):
+    name = "cscore"
+    version = "2024.3.2"
+    settings = "os", "arch", "build_type"
+    options = {"shared": [True, False]}
+    python_requires = "wpireq/0.1"
+    python_requires_extend = "wpireq.Wpibase"
+
+    def requirements(self):
+        self.requires(f"wpiutil/{self.version}")
+        self.requires(f"wpinet/{self.version}")
+        self.requires("opencv/4.8.0-2")
+
+    def build(self):
+        header_url, lib_url = super().get_wpi_urls(
+            self.name,
+            self.version,
+            self.settings.os,
+            self.settings.arch,
+            self.options.shared,
+            self.settings.build_type,
+        )
+        get(self, header_url)
+        get(self, lib_url)
+
+    def package(self):
+        super().copy_common_files()
+
+    def package_info(self):
+        if self.settings.os == "Windows" and self.settings.build_type == "Debug":
+            lib_postfix = "d"
+        else:
+            lib_postfix = ""
+        self.cpp_info.libs = [f"cscore{lib_postfix}"]
