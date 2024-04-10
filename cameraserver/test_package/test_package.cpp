6298f2c8dd608bca22d8ae00b17a8a877517f176
Drew Williams <williams.r.drew@gmail.com>
2024-04-10 10:02:42 -0400

    added cameraserver

---
+++ cameraserver/test_package/test_package.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "cameraserver/CameraServer.h"
+#include <fmt/core.h>
+#include <networktables/NetworkTableInstance.h>
+
+
+int main(int argc, char *argv[]) {
+  // start NetworkTables
+  auto ntinst = nt::NetworkTableInstance::GetDefault();
+  ntinst.StartServer();
+
+  auto camera = frc::CameraServer::StartAutomaticCapture();
+  return 0;
+}
