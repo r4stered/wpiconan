9eb9868055e8dbe89f1460ad36585fdf33f99a66
Drew Williams <williams.r.drew@gmail.com>
2024-04-10 08:54:12 -0400

    added imgui

---
+++ thirdparty/imgui/conanfile.py
@@ -0,0 +1,56 @@
+import os
+from conan import ConanFile
+from conan.tools.files import get, copy
+
+
+class ImguiConan(ConanFile):
+    name = "imgui"
+    version = "1.89.9-1"
+    settings = "os", "arch", "build_type"
+    python_requires = "wpireq/0.1"
+    python_requires_extend = "wpireq.Wpibase"
+
+    def build(self):
+        _os = self.get_os_name_for_url(self.settings.os)
+        _arch = self.get_arch_name_for_url(self.settings.arch)
+        _build_type = self.get_build_type_name_for_url(self.settings.build_type)
+        if _os == "osx":
+            _arch = "universal"
+
+        base_url = f"https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/thirdparty/frc2024/{self.name}/{self.version}"
+        header_url = f"{base_url}/{self.name}-{self.version}-headers.zip"
+        lib_url = (
+            f"{base_url}/{self.name}-{self.version}-{_os}{_arch}static{_build_type}.zip"
+        )
+
+        get(self, header_url)
+        get(self, lib_url)
+
+    def package(self):
+        copy(
+            self, "*.h", self.build_folder, os.path.join(self.package_folder, "include")
+        )
+        copy(
+            self,
+            "*.a",
+            self.build_folder,
+            os.path.join(self.package_folder, "lib"),
+            False,
+        )
+        copy(
+            self,
+            "*.lib",
+            self.build_folder,
+            os.path.join(self.package_folder, "lib"),
+            False,
+        )
+        copy(
+            self,
+            "*.pdb",
+            self.build_folder,
+            os.path.join(self.package_folder, "lib"),
+            False,
+        )
+
+    def package_info(self):
+        self.cpp_info.libs = ["imgui"]
